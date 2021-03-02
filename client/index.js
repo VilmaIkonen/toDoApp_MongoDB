c9e9e23fa5c21cb9aafaff6e7a1e63a5e08fec53
VilmaIkonen <vilma.r.ikonen@gmail.com>
2021-03-02 08:59:59 +0200

    Data from MongoDB shown on frontend table

--- client/index.js
+++ client/index.js
@@ -22,26 +22,21 @@ document.addEventListener('DOMContentLoaded', () => {
 document.querySelector('#getAll').addEventListener('click',  () => {
 	fetch('http://localhost:5000/getAll')
 	.then(response => response.json())
-	.then(data => loadHTMLTable(data['data']));    
+	.then(todos => loadData(todos));    
 });
 
-// function loadHTMLTable(data) {
-// 	const table = document.querySelector('table tbody');
+const loadData = (todos) => {
+	const table = document.querySelector('table tbody');
 
-// 	if (data.length === 0) {
-// 			table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
-// 			return;
-// 	}
+	let tableHtml = "";
 
-// 	let tableHtml = "";
+	todos.forEach(({todo, date_added}) => {
+		tableHtml += "<tr>";
+		tableHtml += `<td>${todo}</td>`;
+		tableHtml += `<td>${new Date(date_added).toLocaleDateString('en-GB')}</td>`;
+		tableHtml += "</tr>";
+	});
 
-// 	data.forEach(function ({todo, date_added}) {
-// 			tableHtml += "<tr>";
-// 			tableHtml += `<td>${todo}</td>`;
-// 			tableHtml += `<td>${new Date(date_added).toLocaleDateString('en-GB')}</td>`;
-// 			tableHtml += "</tr>";
-// 	});
-
-// 	table.innerHTML = tableHtml;
-// }
+	table.innerHTML = tableHtml;
+}
 
